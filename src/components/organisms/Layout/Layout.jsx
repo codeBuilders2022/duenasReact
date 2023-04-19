87b65dc282505426b31754c4ff77fe03aa73fc69
codeBuilders2022 <codebuilders2022@gmail.com>
2023-04-18 19:09:26 -0600

    cotizador

--- src/components/organisms/Layout/Layout.jsx
+++ src/components/organisms/Layout/Layout.jsx
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
     <>
       <Navbar />
       <Header />
-      <NavLink to="https://wa.me/+5215213332344601" className={"ws"}>
+      <NavLink to="https://wa.me/+5215213332344601" target="_blank" rel="noreferrer" className={"ws"}>
         <img src={Whatsapp} alt="" />
       </NavLink>
       {children}
